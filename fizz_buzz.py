522a2565633185becf365ec456fd998ad5c7a824
Joseph Colca <josephcolca@Josephs-Air.localdomain>
2018-09-18 13:50:31 -0500

    Initial commit for fizz_buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+number = int(input("Please enter a number: "))
+
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "Fizz Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return "Sorry Try Again"
+
+print(fizz_buzz(number))
